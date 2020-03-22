286568f26296d90fbfcecb3f42878ea7767f7c87
Jesus-cm1907 <jesuka2000@gmail.com>
2020-03-22 21:27:08 +0100

    Test5

--- TestFuncion5Test.java
+++ TestFuncion5Test.java
@@ -18,27 +18,33 @@ public class TestFuncion5Test {
     }
 
     @BeforeEach
-	private void cont() {
-		contpru++;
-		System.out.println("Empezamos la prueba "+contpru);
-	}
+    private void cont() {
+        contpru++;
+        System.out.println("Empezamos la prueba "+contpru);
+    }
 
-	@AfterEach
-	private void noveces() {
-		System.out.println("Se han realizado "+ contpru +" pruebas");
-	}
+    @AfterEach
+    private void noveces() {
+        System.out.println("Se han realizado "+ contpru +" pruebas");
+    }
 
-	@AfterAll
-	static void acabar() {
-		contpru = 0;
-		funcion5 = null;
-		System.out.println("Todas las pruebas realizadas");
-	}
-
+    @AfterAll
+    static void acabar() {
+        contpru = 0;
+        funcion5 = null;
+        System.out.println("Todas las pruebas realizadas");
+    }
+    //Jose Antonio Reina
     @DisplayName("Test de Caja Negra con cadena")
     @Test
     void test1() {
         assertEquals(195, funcion5.Funcion5(
                 "JoséAntonioReina 22/10/2001, Pablo José Cerero 01/10/1995, Jesus Cansino 11/12/2000"));
+    }
+    //Jesus Cansino Molina
+    @DisplayName("Test de caja Negra con Valor Nulo")
+    @Test
+    void test2() {
+    assertNull(funcion.funcion5(null));
     }
 }
