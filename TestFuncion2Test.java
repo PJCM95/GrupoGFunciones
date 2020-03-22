b8ca544b3e489c3cb05484c984a227e15aedf008
Josearemos <josearemos01@gmail.com>
2020-03-22 20:09:00 +0100

    1a Prueba Funcion 2

---
+++ TestFuncion2Test.java
@@ -0,0 +1,50 @@
+package GrupoG;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class TestFuncion2Test {
+
+    static Funcion2 funcion2 = null;
+    static int contpru = 0;
+
+    @BeforeAll
+    static void preparacion() {
+        System.out.println("Empezamos los Test");
+        funcion2 = new Funcion2();
+    }
+
+    @BeforeEach
+	private void cont() {
+		contpru++;
+		System.out.println("Empezamos la prueba "+contpru);
+	}
+
+	@AfterEach
+	private void noveces() {
+		System.out.println("Se han realizado "+ contpru +" pruebas");
+	}
+
+	@AfterAll
+	static void acabar() {
+		contpru = 0;
+		funcion2 = null;
+		System.out.println("Todas las pruebas realizadas");
+	}
+   
+
+    	//José Antonio Reina Montes
+    @DisplayName("Test de Caja Negra Con Un Valor Nulo")
+    @Test
+    void test1() {
+        assertNull(funcion2.Funcion2(null));
+    }
+
+ 
+}
+
