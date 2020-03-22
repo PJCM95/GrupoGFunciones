2fa7b87c05cb19992a32712df2424be11fd414ca
Josearemos <josearemos01@gmail.com>
2020-03-22 21:24:41 +0100

    1a prueba funcion6

---
+++ TestFuncion6Test.java
@@ -0,0 +1,44 @@
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class TestFuncion5Test {
+
+    static Funcion5 funcion6 = null;
+    static int contpru = 0;
+
+    @BeforeAll
+    static void preparacion() {
+        System.out.println("Empezamos los Test");
+        funcion6 = new Funcion6();
+    }
+
+    @BeforeEach
+	private void cont() {
+		contpru++;
+		System.out.println("Empezamos la prueba "+contpru);
+	}
+
+	@AfterEach
+	private void noveces() {
+		System.out.println("Se han realizado "+ contpru +" pruebas");
+	}
+
+	@AfterAll
+	static void acabar() {
+		contpru = 0;
+		funcion6 = null;
+		System.out.println("Todas las pruebas realizadas");
+	}
+	//José Antonio Reina Montes
+	@DisplayName("Test de analisis de limites")
+    @Test
+    void test1() {
+        assertNull(0, 0, 0, 0, funcion6.Funcion6(null));
+    } 
+}
