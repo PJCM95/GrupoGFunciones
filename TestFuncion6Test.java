8200baeddf798a93e0f249ece9637b3ba3afe322
Jesus-cm1907 <jesuka2000@gmail.com>
2020-03-22 21:36:16 +0100

    Test6

--- TestFuncion6Test.java
+++ TestFuncion6Test.java
@@ -1,4 +1,3 @@
-
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
@@ -19,26 +18,32 @@ public class TestFuncion5Test {
     }
 
     @BeforeEach
-	private void cont() {
-		contpru++;
-		System.out.println("Empezamos la prueba "+contpru);
-	}
+    private void cont() {
+        contpru++;
+        System.out.println("Empezamos la prueba "+contpru);
+    }
 
-	@AfterEach
-	private void noveces() {
-		System.out.println("Se han realizado "+ contpru +" pruebas");
-	}
+    @AfterEach
+    private void noveces() {
+        System.out.println("Se han realizado "+ contpru +" pruebas");
+    }
 
-	@AfterAll
-	static void acabar() {
-		contpru = 0;
-		funcion6 = null;
-		System.out.println("Todas las pruebas realizadas");
-	}
-	//José Antonio Reina Montes
-	@DisplayName("Test de analisis de limites")
+    @AfterAll
+    static void acabar() {
+        contpru = 0;
+        funcion6 = null;
+        System.out.println("Todas las pruebas realizadas");
+    }
+    //JosÃ© Antonio Reina Montes
+    @DisplayName("Test de analisis de limites")
     @Test
     void test1() {
         assertNull(0, 0, 0, 0, funcion6.Funcion6(null));
     } 
+//Jesus Cansino Molina
+@DisplayName("Test de Caja Negra con cadena")
+    @Test
+    void testFuncion6() {
+        assertArrayEquals({6, 72, 12, 1}, funcion.funcion6({1, 2, 2, 1}));
+    }
 }
